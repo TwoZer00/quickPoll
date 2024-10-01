228259003b4639f646051969ea245407fea590b9
twozer00 <leobardo.21@outlook.es>
2024-09-30 21:07:36 -0700

    ads fix

--- src/pages/InitAuth.jsx
+++ src/pages/InitAuth.jsx
@@ -35,18 +35,16 @@ export default function InitAuth () {
           {message?.message}
         </Alert>
       </Snackbar>
-      <script
-        async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7731037445831235'
-        crossOrigin='anonymous'
-      />
-      <ins
-        className='adsbygoogle' style='display:block;'
-        data-ad-client='ca-pub-7731037445831235' data-ad-slot='5542566407' data-ad-format='auto'
-        data-full-width-responsive='true'
-      />
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
+      <amp-ad
+        width='100vw' height='320'
+        type='adsense'
+        data-ad-client='ca-pub-7731037445831235'
+        data-ad-slot='5542566407'
+        data-auto-format='rspv'
+        data-full-width=''
+      >
+        <div overflow='' />
+      </amp-ad>
     </>
   )
 }

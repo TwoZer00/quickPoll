3d8426bde441036c374600d0d3da46467449c691
Leobardo Verdugo <leobardo@twozer00.dev>
2025-07-05 14:44:19 -0700

    fix home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home () {
   useTitle({ title: 'QuickPoll - Create your polls, share it and see the results' })
   return (
     <>
-      <Stack direction='column' height='100dvh' width='100dvw'>
+      <Stack direction='column' flex={1}>
         <Box component='main' flex={1} display='flex' justifyContent='center' flexDirection='column' alignItems='center'>
           <Typography variant='h1' fontWeight={500}>QuickPoll</Typography>
           <Typography variant='body1' align='center'>Create quick poll, share it and see the results in real time.</Typography>
